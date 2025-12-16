636e20f7e49e5ea46abaaf1e187aa2ead637a228
sylvi <20102981@mail.wit.ie>
2025-12-16 13:59:43 +0000

    Added movies router; get movies from TMDB

---
+++ movies-api/api/movies/index.js
@@ -0,0 +1,13 @@
+import asyncHandler from 'express-async-handler';
+import { getMovies } from '../tmdb-api'; 
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/discover', asyncHandler(async (req, res) => {
+    const discoverMovies = await getMovies();
+    res.status(200).json(discoverMovies);
+}));
+
+
+export default router;
