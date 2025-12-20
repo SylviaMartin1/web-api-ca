356e0df186af63ae3e9d46d3a58cf3f6e88a4f29
sylvi <20102981@mail.wit.ie>
2025-12-20 16:59:48 +0000

    Create other mongoose model and router

---
+++ movies-api/api/reviews/index.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import Review from './reviewModel';
+import asyncHandler from 'express-async-handler';
+import authenticate from '../../authenticate';
+
+const router = express.Router();
+
+router.use(authenticate);
+
+
+// Get all reviews of a specific user
+router.get('/', async (req, res) => {
+    console.log(req.user);
+    const reviews = await Review.find({ userId: `${req.user._id}`});
+    res.status(200).json(reviews);
+});
+
+// Add a review
+router.post('/', asyncHandler(async (req, res) => {
+    const newReview = req.body;
+    newReview.userId = req.user._id;
+    const review = await Review(newReview).save();
+    res.status(201).json(review);
+}));
+
+// Update a review
+router.put('/:id', async (req, res) => {
+    if (req.body._id) delete req.body._id;
+    const result = await Review.updateOne({
+        _id: req.params.id,
+    }, req.body);
+    if (result.matchedCount) {
+        res.status(200).json({ code:200, msg: 'Review Updated Successfully' });
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find Review' });
+    }
+});
+
+// Delete a review
+router.delete('/:id', async (req, res) => {
+    const result = await Review.deleteOne({
+        _id: req.params.id,
+    });
+    if (result.deletedCount) {
+        res.status(204).json();
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find Review' });
+    }
+});
+
+// Get all reviews for a specific movie
+router.get('/movie/:id/reviews', asyncHandler(async (req, res) => {
+    const reviews = await Review.find({ movieId: Number(req.params.id) });
+    res.status(200).json(reviews);
+}));
+
+export default router;
